bc3f82b08db0585f9b3f241836f4e5fc4ab192bc
Polina Khisamutdinova <polina.hisa@mail.ru>
2025-09-27 21:33:57 +0300

    Task3

---
+++ Tyuiu.KhisamutdinovaPR.Sprint1.Task3.V4.Lib/DataService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KhisamutdinovaPR.Sprint1.Task3.V4.Lib
+{
+    // Предполагаемый интерфейс: ISprint1Task3V4
+    public class DataService : ISprint1Task3V4
+    {
+        /// <summary>
+        /// Вычисляет стоимость покупки: (цена тетради + цена обложки) * количество
+        /// </summary>
+        public double CalculatePurchase(int count)
+        {
+            double priceNotebook = 2.75;
+            double priceCover = 0.5;
+
+            double total = (priceNotebook + priceCover) * count;
+            return Math.Round(total, 3);
+        }
+
+        public double PurchaseAmount(double priceNotebook, double priceCover, int quantity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tyuiu.KhisamutdinovaPR.Sprint1.Task3.V4.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.KhisamutdinovaPR.Sprint1.Task3.V4.Lib;
+
+namespace Tyuiu.KhisamutdinovaPR.Sprint1.Task3.V4.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CalculatePurchase_ReturnsExpected()
+        {
+            var ds = new DataService();
+
+            // Пример: 7 комплектов => (2.75 + 0.5) * 7 = 22.75
+            double expected = 22.75;
+            double actual = ds.CalculatePurchase(7);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
