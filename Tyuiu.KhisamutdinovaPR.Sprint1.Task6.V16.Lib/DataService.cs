dcf3974fc50ed980d00001cdce107aa8137c6759
Polina Khisamutdinova <polina.hisa@mail.ru>
2025-09-27 22:31:03 +0300

    Task6

--- Tyuiu.KhisamutdinovaPR.Sprint1.Task6.V16.Lib/DataService.cs
+++ Tyuiu.KhisamutdinovaPR.Sprint1.Task6.V16.Lib/DataService.cs
@@ -4,6 +4,11 @@ namespace Tyuiu.KhisamutdinovaPR.Sprint1.Task6.V16.Lib
 {
     public class DataService : ISprint1Task6V16
     {
+        public bool CheckSpecSymbols(string value)
+        {
+            throw new NotImplementedException();
+        }
+
         // Проверка: строка содержит и "!" и "?"
         public bool CheckSymbols(string value)
         {
