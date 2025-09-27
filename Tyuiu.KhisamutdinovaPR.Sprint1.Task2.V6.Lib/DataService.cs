0d9dee51497478d8c558d237461cc0f04ad2936c
Polina Khisamutdinova <polina.hisa@mail.ru>
2025-09-27 21:16:41 +0300

    Task2

---
+++ Tyuiu.KhisamutdinovaPR.Sprint1.Task2.V6.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.KhisamutdinovaPR.Sprint1.Task2.V6.Lib
+{
+    // Предполагаемый интерфейс: ISprint1Task2V6
+    public class DataService : ISprint1Task2V6
+    {
+        /// <summary>
+        /// Переводит метры в километры с округлением до 3 знаков
+        /// </summary>
+        public double ConvertToKm(int meters)
+        {
+            double km = meters / 1000.0;
+            return Math.Round(km, 3);
+        }
+    }
+}
---
+++ Tyuiu.KhisamutdinovaPR.Sprint1.Task2.V6.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.KhisamutdinovaPR.Sprint1.Task2.V6.Lib;
+
+namespace Tyuiu.KhisamutdinovaPR.Sprint1.Task2.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ConvertMetersToKm_CorrectResult()
+        {
+            var ds = new DataService();
+
+            // 1234 м = 1.234 км (округляем до 3 знаков)
+            double expected = 1.234;
+            double actual = ds.ConvertToKm(1234);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
