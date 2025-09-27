40a24bc9cc50834b79704a4b3d5fe2cc5baaa3d2
Polina Khisamutdinova <polina.hisa@mail.ru>
2025-09-27 21:44:56 +0300

    Task3

--- Tyuiu.KhisamutdinovaPR.Sprint1.Task3.V4.Lib/DataService.cs
+++ Tyuiu.KhisamutdinovaPR.Sprint1.Task3.V4.Lib/DataService.cs
@@ -5,8 +5,7 @@ namespace Tyuiu.KhisamutdinovaPR.Sprint1.Task3.V4.Lib
 {
     public class DataService : ISprint1Task3V4
     {
-        // Возвращает стоимость покупки для count комплектов.
-        // 1 комплект: тетрадь 3.5 руб + обложка 0.5 руб = 4.0 руб
+        // 1 комплект = тетрадь (3.5 руб) + обложка (0.5 руб) = 4.0 руб
         public double CalculatePurchase(int count)
         {
             double total = 4.0 * count;
--- Tyuiu.KhisamutdinovaPR.Sprint1.Task3.V4/Program.cs
+++ Tyuiu.KhisamutdinovaPR.Sprint1.Task3.V4/Program.cs
@@ -9,14 +9,33 @@ namespace Tyuiu.KhisamutdinovaPR.Sprint1.Task3.V4
         static void Main(string[] args)
         {
             Console.Title = "Спринт #1 | Выполнила: Хисамутдинова Полина Руслановна | ПКТб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнила: Хисамутдинова П. Р. | ПКТб-25-1                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Вычислить стоимость покупки, состоящей из нескольких тетрадей и такого  *");
+            Console.WriteLine("* же количества обложек. Цена тетради = 3.5 руб, обложки = 0.5 руб.       *");
+            Console.WriteLine("* Ответ округлить до 3 знаков после запятой.                              *");
+            Console.WriteLine("***************************************************************************");
 
-            Console.Write("Введите количество комплектов: ");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.Write("Введите количество комплектов (шт): ");
             int count = int.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
 
             var ds = new DataService();
             double result = ds.CalculatePurchase(count);
 
-            Console.WriteLine(result.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("Стоимость покупки: " + result.ToString("F3", CultureInfo.InvariantCulture) + " руб.");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
             Console.ReadKey();
         }
     }
